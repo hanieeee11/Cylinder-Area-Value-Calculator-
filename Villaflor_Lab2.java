e35d970175d599b2f1b48dfad0135fa9d4e526bc
Jay Han Louie's Villaflor <yukinachanvillaflor@gmail.com>
2024-09-03 02:34:44 +0800

    Update Villaflor_Lab2.java

--- Villaflor_Lab2.java
+++ Villaflor_Lab2.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 class Cylinder{
     // This will be our base class
 }
-class CylinderCalculator extends Cylinder{ /*To make the code much shorter I only made one class that extends the base class, I also want to make the structure of the code more clean and OOP-based */
-    public static double area(double x, double y){ // I've put parameters x,y sso there's no need to declare the variables for radius and height
-        return 2 * Math.PI * x * (x + y); 
+/*To make the code much shorter I only made one class that extends the base class, I also want to make the structure of the code more clean and OOP-based */
+class CylinderCalculator extends Cylinder{ 
+    public static pi = Math.PI;
+    public double area(double x, double y){ // I've put parameters x,y sso there's no need to declare the variables for radius and height
+        return 2 * pi * x * (x + y); 
     }
-
-    public static double vol(double x, double y){
-        return Math.PI * Math.pow(x, 2) * y;
+    public double vol(double x, double y){
+        return pi * (x * x) * y;
     }
 }
-
-class Main {
+// Main class inherits the attributes of the previous class.
+class Main extends CylinderCalculator{
     public static void main(String[] args) {
         Scanner dimensions = new Scanner(System.in);
         CylinderCalculator calc = new CylinderCalculator(); //Creating object 
@@ -28,8 +29,8 @@ class Main {
         double volume = calc.vol(rad,height);
 
         //Print/display the answer
-        System.out.println("the area is: " + area);
-        System.out.println("the volume is: " + volume);
+        System.out.println("The area of the cylinder is: " + area);
+        System.out.println("The volume of the cylinder is: " + volume);
 
         dimensions.close(); //To close the scanner.
     }
